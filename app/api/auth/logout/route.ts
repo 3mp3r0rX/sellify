30ea246344e6c05804bd7b7300c4c776b776b0c4
veirtex <veirtex@proton.me>
2024-09-15 02:49:24 +0300

    deleted old apis from nextjs

--- app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import cookie from 'cookie';
-
-export async function POST() {
-  const response = NextResponse.json({ message: 'Logout successful' });
-  response.headers.append('Set-Cookie', cookie.serialize('token', '', { httpOnly: true, path: '/', expires: new Date(0) }));
-  
-  return response;
-}
