7219c0a0f695803a07b7c67cd130e166ab506526
veirtex <veirtex@proton.me>
2024-09-14 19:38:48 +0300

    todo

---
+++ server/middleware/auth.go
@@ -0,0 +1,12 @@
+package middleware
+
+// import (
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/gofiber/fiber/v2/middleware/session"
+// )
+
+// func Authentication(store *session.Store) fiber.Handler{
+// 	return func(c *fiber.Ctx) error {
+
+// 	}
+// }
