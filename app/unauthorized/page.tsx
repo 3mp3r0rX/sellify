6ba467c3ca0927bd4a800b2b8ee838e4bed7f142
veirtex <veirtex@proton.me>
2024-09-20 12:10:17 +0300

    new page for unauthorized access

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+const unauthorized = () => {
+    return (
+        <div className="bg-black h-[800px]">
+            <h1 className="text-center text-white text-[50px] pt-40">what are u doing here, YOU ARE NOT AN ADMIN</h1>
+        </div>
+    )
+}
+
+export default unauthorized;
